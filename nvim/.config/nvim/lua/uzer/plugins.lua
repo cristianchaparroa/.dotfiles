0e3005851898a4399f4ff631a08a9617325bdd52
cristianchaparroa <cristianchaparroa@gmail.com>
2022-01-19 15:43:53 -0500

    feat: organize plugins

--- nvim/.config/nvim/lua/uzer/plugins.lua
+++ nvim/.config/nvim/lua/uzer/plugins.lua
@@ -1,81 +1,75 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim'
+    fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+    execute 'packadd packer.nvim'
 end
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  -- Dracula theme
-  use {'dracula/vim', as = 'dracula'} 
-  use {'tiagovla/tokyodark.nvim', as = 'tokyodark'}
+    -- Dracula theme
+    use {'dracula/vim', as = 'dracula'} 
+    use {'tiagovla/tokyodark.nvim', as = 'tokyodark'}
 
-  -- Telescope
-  use {
-    'nvim-telescope/telescope.nvim',
-     requires = { { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } },
-  }
-  
-  -- Telescope Extensions
-  use 'kyazdani42/nvim-web-devicons'
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-  use { "nvim-telescope/telescope-file-browser.nvim" }
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  -- Lightline
-  -- use 'itchyny/lightline.vim'
-  use {
-   'nvim-lualine/lualine.nvim',
-   requires = {'kyazdani42/nvim-web-devicons', opt = true}
-  }
-
-
-  -- Vim-fugitive
-  use 'tpope/vim-fugitive'
-
-  -- GitSigns
-  use 'lewis6991/gitsigns.nvim'
-
-  -- LSP
-  use 'neovim/nvim-lspconfig'
-
-  -- GO
-  use 'fatih/vim-go'
-
-  -- Autocompletion
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'L3MON4D3/LuaSnip'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'onsails/lspkind-nvim'
-
-  -- Treesitter
-  use {'nvim-treesitter/nvim-treesitter',  run =  ':TSUpdate' }
-
-
-  -- Nvim TREE
-  -- use 'kyazdani42/nvim-tree.lua'
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    -- Telescope
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } },
     }
-  }
+  
+    -- Telescope Extensions
+    use 'kyazdani42/nvim-web-devicons'
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    use { "nvim-telescope/telescope-file-browser.nvim" }
+  
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = {'kyazdani42/nvim-web-devicons', opt = true}
+    }
+    
+    -- TABLINE to use buffers as tabs
+    -- http://neovimcraft.com/plugin/kdheepak/tabline.nvim/index.html
+    use 'kdheepak/tabline.nvim'
+    
 
-  use {'neoclide/coc.nvim', branch = 'release'}
+    -- TREE for nvim
+    use {
+        'kyazdani42/nvim-tree.lua',
+        requires = {
+            'kyazdani42/nvim-web-devicons', -- optional, for file icon
+        }
+    }   
+    
+    -- GIT
+    use 'tpope/vim-fugitive'        -- Vim-fugitive   
+    use 'lewis6991/gitsigns.nvim'   -- GitSigns
 
-  -- http://neovimcraft.com/plugin/kdheepak/tabline.nvim/index.html
-  use 'kdheepak/tabline.nvim'
+    -- LANGUAGE SERVER PROTOCOL
+    use 'neovim/nvim-lspconfig'
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+
+    -- Treesitter is a parser generator tool and incremental parsing library.
+    use {'nvim-treesitter/nvim-treesitter',  run =  ':TSUpdate' }
+
+    -- LANGUAGES 
+    use 'fatih/vim-go' -- GO
+
+    -- AUTOCOMPLETION
+    use 'hrsh7th/nvim-cmp'                          -- Completion framework 
+    use 'hrsh7th/cmp-nvim-lsp'                      -- LSP copletion source for nvim-cmp
+    use {'neoclide/coc.nvim', branch = 'release'}   -- conquer of completion
+    use 'L3MON4D3/LuaSnip'                          -- snippet engine
+    use 'saadparwaiz1/cmp_luasnip'                  -- luasnip completion source for nvim-cmp
+    use 'onsails/lspkind-nvim'                      -- adds pictograms to nvim built-in lsp
+
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
