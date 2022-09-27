c799cafe06175e2d1928fac4e6ad734380885945
cristianchaparroa <cristianchaparroa@gmail.com>
2022-09-26 21:42:44 -0500

    copy and paste

--- nvim/.config/nvim/lua/uzer/maps.lua
+++ nvim/.config/nvim/lua/uzer/maps.lua
@@ -1,2 +1,25 @@
 -- set leader to space
 vim.g.mapleader = ' '
+
+local map = vim.api.nvim_set_keymap
+local noremap = {noremap = true}
+
+-- Reference: https://www.linuxfordevices.com/tutorials/linux/copy-paste-multiple-registers-in-vim
+
+-- Set a key-mapping for copy and pasting to the system clipboard
+
+map('v', '<leader>y', '"+y', noremap)
+map('n', '<leader>p', '"+p', noremap)
+
+
+-- Set key-mapping for dealing with two alphabetical registers easily
+-- Two does the work for me, you can set more
+--
+map('v', '<leader>a', '"ay', noremap)
+map('v', '<leader>A', '"Ay', noremap)
+map('n', '<leader>a', '"api', noremap)
+
+map('v', '<leader>x', '"xy', noremap)
+map('v', '<leader>X', '"Xy', noremap)
+map('n', '<leader>x', '"xpcall', noremap)
+
