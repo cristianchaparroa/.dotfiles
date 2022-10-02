63d1736b4834d966a0beac8747ee634063660f9c
Cristian <cristianchaparroa@users.noreply.github.com>
2022-10-02 17:09:55 -0500

    fix updates (#27)

--- nvim/.config/nvim/lua/uzer/lspsaga/maps.lua
+++ nvim/.config/nvim/lua/uzer/lspsaga/maps.lua
@@ -6,5 +6,5 @@ map("n", "gh",      "<cmd>lua require'lspsaga.provider'.lsp_finder()<CR>",      
 map("n", "K",       "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>",             opts)   -- sgiw giver doc
 map("n", "<C-f>",   "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>",     opts)   -- scroll down hover doc or scroll in definition preview
 map("n", "<C-b>",   "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>",    opts)   -- scroll up hover doc
-map("n", "sh",      "<cmd>lua require('lspsaga.signaturehelp').signature_help()<CR>",       opts)   -- show signature help
+map("n", "<leader>s",      "<cmd>lua require('lspsaga.signaturehelp').signature_help()<CR>",       opts)   -- show signature help
 map("n", "mm",      "<cmd>lua require('lspsaga.rename').rename()<CR>", {noremap = true})            -- rename
