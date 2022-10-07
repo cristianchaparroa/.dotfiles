4311da749c9407dc7818f74dd2b83f7e4975c54d
Cristian <cristianchaparroa@users.noreply.github.com>
2022-10-07 18:04:38 -0500

    Debian/install rust and solang support (#30)
    * install rust and rust-anaylzer
    
    * solang instalation

--- bin/linux/debian/languages.sh
+++ bin/linux/debian/languages.sh
@@ -34,9 +34,51 @@ function install_go() {
     go install golang.org/x/tools/gopls@latest
 }
 
+function install_rust() { 
+
+  
+    echo "#RUST" >> ~/.config/fish/config.fish
+    curl https://sh.rustup.rs -sSf | sh -s -- -y 
+    echo "fish_add_path $HOME/.cargo/bin" >> ~/.config/fish/config.fish 
+    source ~/.config/fish/config.fish
+
+    $HOME/.cargo/bin/rustup component add rls rust-analysis rust-src
+
+    mkdir -p ~/.local/bin 
+
+    mkdir -p ~/.local/bincurl -L https://github.com/rust-lang/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz | gunzip -c - > ~/.local/bin/rust-analyzer
+
+    chmod +x ~/.local/bin/rust-analyzer
+    
+    source ~/.config/fish/config.fish
+}
+
+function install_solang() {
+    mkdir ~/src/llvm     
+    cd  ~/src/llvm
+
+    git clone --depth 1 --branch solana-rustc/13.0-2021-08-08 https://github.com/solana-labs/llvm-project
+    cd llvm-project
+
+    cmake -G Ninja -DLLVM_ENABLE_ASSERTIONS=On '-DLLVM_ENABLE_PROJECTS=clang;lld'  \
+        -DLLVM_ENABLE_TERMINFO=Off -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_INSTALL_PREFIX=installdir -B build llvm
+    cmake --build build --target install 
+     
+
+    echo -e "fish_add_path $HOME/src/llvm/llvm-project/installdir/bin/ \n" >> ~/.config/fish/config.fish
+
+    export PATH=$HOME/src/llvm/llvm-project/bin:$PATH
+    cargo install solang    
+
+}
+
 function main() {
     install_lua
     install_python
+    install_go
+    install_rust
+    install_solang
 }
 
 main
