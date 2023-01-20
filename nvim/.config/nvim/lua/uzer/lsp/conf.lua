5c8f3298b4c64845063b3c40b8ef3e7ae8b9bf7b
Cristian Chaparro <cristianchaparroa@gmail.com>
2023-01-20 00:48:31 -0500

    fix breaking changes afeter update neovim

--- nvim/.config/nvim/lua/uzer/lsp/conf.lua
+++ nvim/.config/nvim/lua/uzer/lsp/conf.lua
@@ -52,8 +52,8 @@ local on_attach = function(client, bufnr)
     map('n', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts) -- open  manually  the floating window on signature
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 for _, lsp in ipairs(servers) do
